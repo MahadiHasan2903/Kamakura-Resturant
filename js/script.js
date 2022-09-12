7f04e3700e1d5454ef2cb63eb5bf1a847edf4087
MahadiHasan2903 <112541779+MahadiHasan2903@users.noreply.github.com>
2022-09-12 23:58:32 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(".img-slider").slick({
+    slidesToShow: 1,
+    arrows: false,
+    dots: true,
+})
+$(".img1-slider").slick({
+    slidesToShow: 1,
+    arrows: false,
+    dots: true,
+})
+
+$(".img1-slider #slick-slide-control10").html('<div class=" text text-start"><p class="color">1995</p><p>Small Sushi Seller</p></div>');
+$(".img1-slider #slick-slide-control11").html('<div class=" text text-start"><p class="color">1995</p><p>Small Sushi Seller</p></div>');
+$(".img1-slider #slick-slide-control12").html('<div class=" text text-start"><p class="color">1995</p><p>Small Sushi Seller</p></div>');
+$(".img1-slider #slick-slide-control13").html('<div class=" text text-start"><p class="color">1995</p><p>Small Sushi Seller</p></div>');
